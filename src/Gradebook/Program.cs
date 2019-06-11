f616bbe561a3d81b67509627a69ef02ce673f917
Jonathan <Jonathan.Harris@mdpweb.net>
2019-06-11 00:02:51 -0400

    initial commit

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0) {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else {
+                Console.WriteLine("You so dumb, enter a name when you run this");
+            }
+        }
+    }
+}
